06e49f390259bb3a6299548017a9743af179cf1b
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-06-04 01:01:17 +0545

    auth dummy added

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log("server running!");
--- router/route.js
+++ router/route.js
@@ -1,18 +1,55 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const rawDirName = join(__filename, "../");
+const __dirname = dirname(rawDirName);
 export var router = express.Router();
 
+var isAuth = false;
+
+// dummy auth
+let auth = (req, res, next) => {
+  isAuth = true;
+  next();
+};
+
+// index route
 router
   .get("/", (req, res) => {
-    res.send("get in home");
+    res
+      .header("Content-Type", "text/html")
+      .status(200)
+      .sendFile(__dirname + "/pages/index.html");
   })
   .post("/", (req, res) => {
-    res.send("post in home");
+    res.json({
+      error: "bad request",
+    });
   });
 
+  // login route
 router
-  .get("/about", (req, res) => {
-    res.send("get in about");
+  .get("/login", (req, res) => {
+    res.send("send post request here pls");
   })
-  .post("/about", (req, res) => {
-    res.send("post in about");
+  .post("/login", (req, res) => {
+    res.send(req.body.email);
+  });
+
+  // home route
+router
+  .get("/home", auth, (req, res) => {
+    if (isAuth) {
+      res
+        .header("Content-Type", "text/html")
+        .status(200)
+        .sendFile(__dirname + "/pages/home.html");
+    } else {
+      res.send("pls log in");
+    }
+  })
+  .post("/home", (req, res) => {
+    res.send("post in home");
   });
