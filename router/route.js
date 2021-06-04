7ce4e86a5c17f1349c154f109efdb238ef11fd7b
shailesz <sailesh.karki@sagarmatha.edu.np>
2021-06-04 09:08:46 +0545

    login/register routes

--- router/route.js
+++ router/route.js
@@ -1,17 +1,56 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const rawDirName = join(__filename, "../");
 const __dirname = dirname(rawDirName);
 export var router = express.Router();
 
+var users = [];
 var isAuth = false;
 
 // dummy auth
 let auth = (req, res, next) => {
-  isAuth = true;
+  switch (req.path) {
+    case "/login":
+      for (const user of users) {
+        if (user.email === req.body.email) {
+          bcrypt.compare(
+            req.body.password,
+            user.password,
+            function (err, result) {
+              if (result) {
+                isAuth = true;
+              }
+            }
+          );
+        }
+      }
+      break;
+    case "/register":
+      bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(req.body.password, salt, function (err, hash) {
+          users.push({
+            email: req.body.email,
+            password: hash,
+          });
+          fs.promises.writeFile(
+            __dirname + "/db/users.json",
+            JSON.stringify({
+              email: req.body.email,
+              password: hash,
+            })
+          );
+        });
+      });
+
+      break;
+    default:
+      break;
+  }
   next();
 };
 
@@ -29,18 +68,32 @@ router
     });
   });
 
-  // login route
+// register route
+router
+  .get("/register", (req, res) => {
+    res.send("send post request here pls");
+  })
+  .post("/register", auth, (req, res) => {
+    res.redirect("/home");
+  });
+
+// login route
 router
   .get("/login", (req, res) => {
     res.send("send post request here pls");
   })
-  .post("/login", (req, res) => {
-    res.send(req.body.email);
+  .post("/login", auth, (req, res) => {
+    res.redirect("/home");
   });
 
-  // home route
+// display users
+router.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// home route
 router
-  .get("/home", auth, (req, res) => {
+  .get("/home", (req, res) => {
     if (isAuth) {
       res
         .header("Content-Type", "text/html")
